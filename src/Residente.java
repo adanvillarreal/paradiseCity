422c0ff23ffea0a78cd52ce1185b0133acf53ac9
Adan Villarreal <adanvillarreal0@gmail.com>
2017-10-11 16:54:31 -0500

    Created class residente

--- src/Residente.java
+++ src/Residente.java
@@ -1,3 +1,9 @@
 public class Residente{
-    String name;
+    private String name;
+    private int age;
+
+    public Residente(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
