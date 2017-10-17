9be9cdf67174d0108cecb3cc61d68a6e9a2760ae
Adan Villarreal <adanvillarreal0@gmail.com>
2017-10-17 16:07:41 -0500

    Esqueleto completo de Medicina y ProductoGenerico.

--- src/Entidades/ProductoGenerico.java
+++ src/Entidades/ProductoGenerico.java
@@ -1,4 +1,38 @@
 package Entidades;
 
 public class ProductoGenerico {
+    private String nombre;
+    private String descripcion;
+    private int cantidad;
+
+    public ProductoGenerico(String nombre, String descripcion, int cantidad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
 }
